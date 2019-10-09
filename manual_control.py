ffedd0bf5f212bcb2935e875604d4d456c66143b
Nyikovics Máté <nyikovicsmate@gmail.com>
2019-10-09 10:49:25 +0200

    Added counterclockwise loop basic implementation.

--- manual_control.py
+++ manual_control.py
@@ -19,8 +19,8 @@ from gym_duckietown.wrappers import UndistortWrapper
 # from experiments.utils import save_img
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--env-name', default=None)
-parser.add_argument('--map-name', default='udem1')
+parser.add_argument('--env-name', default='Duckietown-loop-v0')
+parser.add_argument('--map-name', default='loop')
 parser.add_argument('--distortion', default=False, action='store_true')
 parser.add_argument('--draw-curve', action='store_true', help='draw the lane following curve')
 parser.add_argument('--draw-bbox', action='store_true', help='draw collision detection bounding boxes')

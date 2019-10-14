9fb1f13af7f5363b7d57a6dfb36929ed6e8f231a
Marci <tmarcikka@gmail.com>
2019-10-14 20:22:01 +0200

    PostProcessing script added.
    It makes the postprocessing steps on recordings stored in the "recordings" folder.

---
+++ postprocess.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+import glob
+import sys
+import cv2
+import os
+
+# Binarization algorithm, with a given original and annotated pair of image
+def binarize_a(img_orig, img_ant):
+    
+    img_hsv = cv2.cvtColor(img_ant,cv2.COLOR_BGR2HSV)
+    
+    lowerBound = (10, 0, 0); #HSV
+    upperBound = (170, 255, 255); #HSV
+    
+    mask = cv2.inRange(img_hsv, lowerBound, upperBound)
+    mask = ~mask
+    
+    result = mask
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(7,7))
+    result = cv2.morphologyEx(result, cv2.MORPH_OPEN, kernel)
+    result = cv2.morphologyEx(result, cv2.MORPH_CLOSE, kernel)
+    return result
+
+def binarize_b(img_orig, img_ant):
+    img_diff = img_orig - img_ant
+    
+    res_gray = cv2.cvtColor(img_diff,cv2.COLOR_BGR2GRAY)
+    res_gray[res_gray > 0] = 255
+    
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+    result = cv2.morphologyEx(res_gray, cv2.MORPH_OPEN, kernel)
+    result = cv2.morphologyEx(result, cv2.MORPH_CLOSE, kernel)
+    return result
+
+# Wrapper function variable; select here the one you want to use
+binarize = binarize_b
+
+# Get the list of available recordings
+annot_raw_list = glob.glob('recordings\*_annot.avi')
+orig_raw_list = glob.glob('recordings\*_orig.avi')
+
+# Check whether original and annotated recordings number match or not
+if len(annot_raw_list) != len(orig_raw_list):
+    print("Length mismatch! No postprocess performed.")
+    sys.exit()
+
+# Iterate and postprocess every recording
+for i in range(len(orig_raw_list)):
+    # Open recordings...
+    cap_orig = cv2.VideoCapture(orig_raw_list[i])
+    cap_annot = cv2.VideoCapture(annot_raw_list[i])
+    if not cap_orig.isOpened() or not cap_annot.isOpened():
+        print("Could not open files! Continuing...")
+        continue
+    
+    # Check whether recordings hold the same number of frames
+    if cap_orig.get(cv2.CAP_PROP_FRAME_COUNT) != cap_annot.get(cv2.CAP_PROP_FRAME_COUNT):
+        print("Different video length encountered! Continuing...")
+        print("DEBUG: orig frames: %i, annot frames: %i" % (cap_orig.get(cv2.CAP_PROP_FRAME_COUNT), cap_annot.get(cv2.CAP_PROP_FRAME_COUNT)))
+        continue
+    
+    # Open VideoWriter Objects
+    fourcc=cv2.VideoWriter_fourcc(*'FFV1')
+    fps=20
+    framesize=(640,480)
+    isColor=True
+    filename_orig, _ = os.path.splitext(orig_raw_list[i])
+    filename_orig = filename_orig + '_pp.avi'
+    if os.path.exists(filename_orig):   # If file exists...
+        os.remove(filename_orig)    # ...delete it
+    vWriter_orig = cv2.VideoWriter(filename_orig, fourcc, fps, framesize, isColor)
+    
+    isColor=False
+    filename_annot, _ = os.path.splitext(annot_raw_list[i])
+    filename_annot = filename_annot + '_pp.avi'
+    if os.path.exists(filename_annot):  # If file exists...
+        os.remove(filename_annot)   # ...delete it
+    vWriter_annot = cv2.VideoWriter(filename_annot, fourcc, fps, framesize, isColor)
+    
+    if not vWriter_orig.isOpened() or not vWriter_annot.isOpened():
+        print("Could not open vide writers! Continuing...")
+        vWriter_annot.release()
+        vWriter_orig.release()
+        continue
+    
+    # Produce output videos
+    print("Processing recording nr. {}...".format(i))
+    while cap_orig.isOpened() and cap_annot.isOpened(): # Iterate through every frame
+        ret_o, frame_o = cap_orig.read()
+        ret_a, frame_a = cap_annot.read()
+        if not ret_o or not ret_a:
+            break
+        
+        # Postprocess original recording: convert from BGR to RGB
+        vWriter_orig.write(cv2.cvtColor(frame_o,cv2.COLOR_BGR2RGB))
+        
+        # Postprocess annotated frame: binarize it
+        annot_binary = binarize(frame_o, frame_a)
+        vWriter_annot.write(annot_binary)
+    
+    print("Processing of recording nr. {} done.".format(i))
+    
+    # Release writer resources
+    vWriter_annot.release()
+    vWriter_orig.release()
+
+print("Post-processing finished!")

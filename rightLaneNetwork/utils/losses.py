911f2bd66d66ae5958894ae5e36ff139fc5b1f2f
Timurlenk07 <tmarcikka@gmail.com>
2020-06-15 10:59:22 +0200

    Added extra type of losses in losses.py (currently unused and untested).

---
+++ rightLaneNetwork/utils/losses.py
@@ -0,0 +1,41 @@
+import torch
+
+
+# source: https://www.kaggle.com/iezepov/fast-iou-scoring-metric-in-pytorch-and-numpy
+def iou_pytorch(outputs: torch.Tensor, labels: torch.Tensor):
+    SMOOTH = 1e-6
+
+    # You can comment out this line if you are passing tensors of equal shape
+    # But if you are passing output from UNet or something it will most probably
+    # be with the BATCH x 1 x H x W shape
+    # outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
+
+    intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
+    union = (outputs | labels).float().sum((1, 2))  # Will be zzero if both are 0
+
+    iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
+
+    thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
+
+    return thresholded.mean()  # Or thresholded.mean() if you are interested in average across the batch
+
+
+# source: https://gist.github.com/weiliu620/52d140b22685cf9552da4899e2160183
+def dice_loss(pred, target):
+    """This definition generalize to real valued pred and target vector.
+This should be differentiable.
+    pred: tensor with first dimension as batch
+    target: tensor with first dimension as batch
+    """
+
+    smooth = 1.
+
+    # have to use contiguous since they may from a torch.view op
+    iflat = pred.contiguous().view(-1)
+    tflat = target.contiguous().view(-1)
+    intersection = (iflat * tflat).sum()
+
+    A_sum = torch.sum(tflat * iflat)
+    B_sum = torch.sum(tflat * tflat)
+
+    return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth))

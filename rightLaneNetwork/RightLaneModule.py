77580c03da8291dd0745d600da17ca5cc00b6ea2
Timurlenk07 <tmarcikka@gmail.com>
2020-08-04 18:45:29 +0200

    Completely reworked baseline solution. Transformation now is only found in myTransforms.py.

--- rightLaneNetwork/RightLaneModule.py
+++ rightLaneNetwork/RightLaneModule.py
@@ -1,69 +1,64 @@
-import functools
 import os
 from argparse import ArgumentParser
 from collections import OrderedDict
 
 import pytorch_lightning as pl
 import torch
-import torch.nn as nn
-from albumentations import Compose, ToGray, Resize, NoOp, HueSaturationValue, Normalize, MotionBlur, RandomSizedCrop, \
-    GaussNoise, OneOf
-from albumentations.pytorch import ToTensorV2
+from pytorch_lightning import seed_everything
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.metrics.functional import accuracy, dice_score, iou
+from torch.nn.functional import cross_entropy
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data import DataLoader
 
 from dataManagement.myDatasets import RightLaneDataset
-from dataManagement.myTransforms import testTransform
+from dataManagement.myTransforms import MyTransform
 from models.FCDenseNet.tiramisu import FCDenseNet57Base, FCDenseNet57Classifier
 
 
 class RightLaneModule(pl.LightningModule):
     def __init__(self, *, dataPath=None, width=160, height=120, gray=False,
-                 augment=False, batchSize=32, lr=1e-3, decay=1e-4, lrRatio=1e3, **kwargs):
+                 augment=False, batch_size=32, lr=1e-3, decay=1e-4, lrRatio=1e3, **kwargs):
         super().__init__()
 
         self.dataPath = dataPath
-        self.trainSet, self.validSet, self.testSet = None, None, None
+        self.dataSets = dict()
 
+        # Network parameters
         self.width, self.height = width, height
         self.grayscale = gray
-        self.criterion = nn.CrossEntropyLoss()
+
+        # Create network parts
+        self.featureExtractor = FCDenseNet57Base()
+        self.classifier = FCDenseNet57Classifier(n_classes=2)
+
+        # Training parameters
         self.augment = augment
-        self.batchSize = batchSize
+        self.batch_size = batch_size
         self.lr = lr
         self.decay = decay
         self.lrRatio = lrRatio
 
-        # save hyperparameters
-        self.save_hyperparameters('width', 'height', 'gray', 'augment', 'batchSize', 'lr', 'decay', 'lrRatio')
-        print(f"The model has the following hyperparameters:")
-        print(self.hparams)
-
-        # Network parts
-        self.featureExtractor = FCDenseNet57Base()
-        self.classifier = FCDenseNet57Classifier(n_classes=2)
+        # Save hyperparameters
+        self.save_hyperparameters('width', 'height', 'gray', 'augment', 'batch_size', 'lr', 'decay', 'lrRatio')
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
 
-        # Data location
-        parser.add_argument('--dataPath', type=str, default='./data')
+        # Network parameters
+        parser.add_argument('--gray', action='store_true', help="Convert input image to grayscale")
+        parser.add_argument('--width', type=int, default=160, help="Resize width of input images of the network")
+        parser.add_argument('--height', type=int, default=120, help="Resize height of input images of the network")
 
-        # parametrize the network
-        parser.add_argument('--gray', action='store_true', help='Convert input image to grayscale')
-        parser.add_argument('-wd', '--width', type=int, default=160, help='Resized input image width')
-        parser.add_argument('-hg', '--height', type=int, default=120, help='Resized input image height')
-
-        # Training hyperparams
-        parser.add_argument('--augment', action='store_true', help='Convert input image to grayscale')
-        parser.add_argument('-lr', '--learningRate', type=float, default=1e-3, help='Base learning rate')
-        parser.add_argument('--decay', type=float, default=1e-4,
-                            help='L2 weight decay value')
-        parser.add_argument('--lrRatio', type=float, default=1000)
-        parser.add_argument('-b', '--batchSize', type=int, default=32, help='Input batch size')
+        # Training parameters
+        parser.add_argument('--augment', action='store_true', help="Use data augmentation on training set")
+        parser.add_argument('-lr', '--learningRate', type=float, default=1e-3, help="Starting learning rate")
+        parser.add_argument('--decay', type=float, default=1e-4, help="L2 weight decay value")
+        parser.add_argument('--lrRatio', type=float, default=1000,
+                            help="Ratio of maximum and minimum of learning rate for cosine LR scheduler")
+        parser.add_argument('-b', '--batch_size', type=int, default=32, help="Input batch size")
 
         return parser
 
@@ -72,48 +67,22 @@ class RightLaneModule(pl.LightningModule):
         x = self.classifier(x)
         return x
 
-    def transform(self, img, label=None):
-        augmentations = Compose([
-            HueSaturationValue(always_apply=True),
-            RandomSizedCrop(min_max_height=(self.height // 2, self.height * 4), height=self.height, width=self.width,
-                            w2h_ratio=self.width / self.height, always_apply=True),
-            OneOf([MotionBlur(p=0.5), GaussNoise(p=0.5)], p=1),
-        ])
-        aug = Compose([
-            augmentations if self.augment else Resize(height=self.height, width=self.width, always_apply=True),
-            ToGray(always_apply=True) if self.grayscale else NoOp(always_apply=True),
-            Normalize(always_apply=True),
-            ToTensorV2(),
-        ])
-
-        if label is not None:
-            # Binarize label
-            label[label != 0] = 1
-
-            augmented = aug(image=img, mask=label)
-            img = augmented['image']
-            label = augmented['mask'].squeeze().long()
-        else:
-            augmented = aug(image=img)
-            img = augmented['image']
-
-        return img, label
-
     def prepare_data(self):
-        testTransform2 = functools.partial(testTransform, width=self.width, height=self.height, gray=self.grayscale)
+        trainTransform = MyTransform(width=self.width, height=self.height, gray=self.grayscale, augment=self.augment)
+        testTransform = MyTransform(width=self.width, height=self.height, gray=self.grayscale, augment=False)
 
-        self.trainSet = RightLaneDataset(os.path.join(self.dataPath, 'train'), self.transform, haveLabels=True)
-        self.validSet = RightLaneDataset(os.path.join(self.dataPath, 'valid'), testTransform2, haveLabels=True)
-        self.testSet = RightLaneDataset(os.path.join(self.dataPath, 'test'), testTransform2, haveLabels=True)
+        self.dataSets['train'] = RightLaneDataset(os.path.join(self.dataPath, 'train'), trainTransform, haveLabels=True)
+        self.dataSets['valid'] = RightLaneDataset(os.path.join(self.dataPath, 'valid'), testTransform, haveLabels=True)
+        self.dataSets['test'] = RightLaneDataset(os.path.join(self.dataPath, 'test'), testTransform, haveLabels=True)
 
     def train_dataloader(self):
-        return DataLoader(self.trainSet, batch_size=self.batchSize, shuffle=True, num_workers=8)
+        return DataLoader(self.dataSets['train'], batch_size=self.batch_size, shuffle=True, num_workers=8)
 
     def val_dataloader(self):
-        return DataLoader(self.validSet, batch_size=self.batchSize, shuffle=False, num_workers=8)
+        return DataLoader(self.dataSets['valid'], batch_size=self.batch_size, shuffle=False, num_workers=8)
 
     def test_dataloader(self):
-        return DataLoader(self.testSet, batch_size=self.batchSize, shuffle=False, num_workers=8)
+        return DataLoader(self.dataSets['test'], batch_size=self.batch_size, shuffle=False, num_workers=8)
 
     def configure_optimizers(self):
         optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=self.decay)
@@ -125,7 +94,7 @@ class RightLaneModule(pl.LightningModule):
 
         # Hálón átpropagáljuk a bemenetet, költséget számítunk
         outputs = self.forward(x)
-        loss = self.criterion(outputs, y)
+        loss = cross_entropy(outputs, y)
 
         # acc
         _, labels_hat = torch.max(outputs, 1)
@@ -151,7 +120,7 @@ class RightLaneModule(pl.LightningModule):
 
         # Hálón átpropagáljuk a bemenetet, költséget számítunk
         outputs = self.forward(x)
-        loss = self.criterion(outputs, y)
+        loss = cross_entropy(outputs, y)
 
         _, labels_hat = torch.max(outputs, 1)
 
@@ -159,7 +128,7 @@ class RightLaneModule(pl.LightningModule):
             'val_loss': loss,
             'acc': accuracy(labels_hat, y),
             'dice': dice_score(outputs, y),
-            'iou': iou(labels_hat, y, remove_bg=True),
+            'iou': iou(labels_hat, y),
             'weight': y.shape[0],
         })
 
@@ -168,25 +137,28 @@ class RightLaneModule(pl.LightningModule):
     def validation_epoch_end(self, outputs):
         val_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
         weight_count = sum(x['weight'] for x in outputs)
-        weighted_acc = torch.stack([x['acc'] * 100.0 * x['weight'] for x in outputs]).sum()
+        weighted_acc = torch.stack([x['acc'] * x['weight'] for x in outputs]).sum()
         weighted_dice = torch.stack([x['dice'] * x['weight'] for x in outputs]).sum()
-        weighted_iou = torch.stack([x['iou'] * 100.0 * x['weight'] for x in outputs]).sum()
-        val_acc = weighted_acc / weight_count
+        weighted_iou = torch.stack([x['iou'] * x['weight'] for x in outputs]).sum()
+        val_acc = weighted_acc / weight_count * 100.0
         val_dice = weighted_dice / weight_count
-        val_iou = weighted_iou / weight_count
+        val_iou = weighted_iou / weight_count * 100.0
 
-        tensorboard_logs = {'val_loss': val_loss,
-                            'val_acc': val_acc,
-                            'val_dice': val_dice,
-                            'val_iou': val_iou}
-        return {'progress_bar': tensorboard_logs, 'log': tensorboard_logs}
+        logs = {
+            'val_loss': val_loss,
+            'val_acc': val_acc,
+            'val_dice': val_dice,
+            'val_iou': val_iou,
+            'step': self.current_epoch,
+        }
+        return {'progress_bar': logs, 'log': logs}
 
     def test_step(self, batch, batch_idx):
         x, y = batch
 
         # Hálón átpropagáljuk a bemenetet, költséget számítunk
         outputs = self.forward(x)
-        loss = self.criterion(outputs, y)
+        loss = cross_entropy(outputs, y)
 
         _, labels_hat = torch.max(outputs, 1)
 
@@ -194,7 +166,7 @@ class RightLaneModule(pl.LightningModule):
             'test_loss': loss,
             'acc': accuracy(labels_hat, y),
             'dice': dice_score(outputs, y),
-            'iou': iou(labels_hat, y, remove_bg=True),
+            'iou': iou(labels_hat, y),
             'weight': y.shape[0],
         })
 
@@ -203,42 +175,69 @@ class RightLaneModule(pl.LightningModule):
     def test_epoch_end(self, outputs):
         test_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
         weight_count = sum(x['weight'] for x in outputs)
-        weighted_acc = torch.stack([x['acc'] * 100.0 * x['weight'] for x in outputs]).sum()
+        weighted_acc = torch.stack([x['acc'] * x['weight'] for x in outputs]).sum()
         weighted_dice = torch.stack([x['dice'] * x['weight'] for x in outputs]).sum()
-        weighted_iou = torch.stack([x['iou'] * 100.0 * x['weight'] for x in outputs]).sum()
-        test_acc = weighted_acc / weight_count
+        weighted_iou = torch.stack([x['iou'] * x['weight'] for x in outputs]).sum()
+        test_acc = weighted_acc / weight_count * 100.0
         test_dice = weighted_dice / weight_count
-        test_iou = weighted_iou / weight_count
+        test_iou = weighted_iou / weight_count * 100.0
 
-        tensorboard_logs = {'test_loss': test_loss,
-                            'test_acc': test_acc,
-                            'test_dice': test_dice,
-                            'test_iou': test_iou}
-        return {'progress_bar': tensorboard_logs, 'log': tensorboard_logs}
+        logs = {
+            'test_loss': test_loss,
+            'test_acc': test_acc,
+            'test_dice': test_dice,
+            'test_iou': test_iou,
+        }
+        return {'progress_bar': logs, 'log': logs}
 
 
-def main(args):
-    model = RightLaneModule(**vars(args))
+def main(args, model_name: str, reproducible: bool, comet: bool):
+    if reproducible:
+        seed_everything(42)
+        args.deterministic = True
+        args.benchmark = True
 
-    if args.comet:
-        comet_logger = pl.loggers.CometLogger(api_key=os.environ.get('COMET_API_KEY'),
-                                              workspace=os.environ.get('COMET_WORKSPACE'),  # Optional
-                                              project_name=os.environ.get('COMET_PROJECT_NAME'),  # Optional
-                                              experiment_name='baseline'  # Optional
-                                              )
+    if comet:
+        comet_logger = pl.loggers.CometLogger(
+            api_key=os.environ.get('COMET_API_KEY'),
+            workspace=os.environ.get('COMET_WORKSPACE'),  # Optional
+            project_name=os.environ.get('COMET_PROJECT_NAME'),  # Optional
+            experiment_name=model_name  # Optional
+        )
         args.logger = comet_logger
+
+    if args.default_root_dir is None:
+        args.default_root_dir = 'results'
+
+    # Save best model
+    model_checkpoint = ModelCheckpoint(
+        filepath=os.path.join(args.default_root_dir, model_name + '.ckpt'),
+        save_top_k=1,
+        verbose=False,
+        monitor='val_iou',
+        mode='max',
+        prefix=model_name + str(os.getpid())
+    )
+    args.checkpoint_callback = model_checkpoint
+
+    model = RightLaneModule(**vars(args))
 
     # Parse all trainer options available from the command line
     trainer = pl.Trainer.from_argparse_args(args)
 
     trainer.fit(model)
 
+    # Reload best model
+    model = RightLaneModule.load_from_checkpoint(model_checkpoint.kth_best_model_path, dataPath=args.dataPath)
+
     # Save checkpoint and weights
-    root_dir = args.default_root_dir if args.default_root_dir is not None else 'results'
-    ckpt_path = os.path.join(root_dir, 'baseline.ckpt')
-    weights_path = os.path.join(root_dir, 'baseline_weights.pth')
+    ckpt_path = os.path.join(args.default_root_dir, model_name + '.ckpt')
+    weights_path = os.path.join(args.default_root_dir, model_name + '_weights.pth')
     trainer.save_checkpoint(ckpt_path)
     torch.save(model.state_dict(), weights_path)
+    if comet:
+        comet_logger.experiment.log_model(model_name + '_ckpt', ckpt_path)
+        comet_logger.experiment.log_model(model_name + '_weights', weights_path)
 
     # Perform testing
     trainer.test(model)
@@ -247,7 +246,14 @@ def main(args):
 if __name__ == '__main__':
     parser = ArgumentParser()
 
+    # Data location
+    parser.add_argument('--dataPath', type=str, help="Path of database root")
+
     parser.add_argument('--comet', action='store_true', help='Define flag in order to use Comet.ml as logger.')
+    parser.add_argument('--model_name', type=str, default='baseline',
+                        help='Model identifier for logging and checkpoints.')
+    parser.add_argument('--reproducible', action='store_true',
+                        help="Set seed to 42 and deterministic and benchmark to True.")
 
     # Add model arguments to parser
     parser = RightLaneModule.add_model_specific_args(parser)
@@ -257,4 +263,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    main(args)
+    main(args, model_name=args.model_name, reproducible=args.reproducible, comet=args.comet)
--- rightLaneNetwork/dataManagement/myTransforms.py
+++ rightLaneNetwork/dataManagement/myTransforms.py
@@ -1,24 +1,34 @@
-from albumentations import Compose, Resize, ToGray, NoOp, Normalize
+from albumentations import Compose, Resize, ToGray, NoOp, Normalize, GaussNoise, HueSaturationValue, RandomSizedCrop, \
+    OneOf, MotionBlur
 from albumentations.pytorch import ToTensorV2
 
 
-def testTransform(img, label=None, width=160, height=120, gray=False):
-    aug = Compose([
-        Resize(height=height, width=width, always_apply=True),
-        ToGray(always_apply=True) if gray else NoOp(always_apply=True),
-        Normalize(always_apply=True),
-        ToTensorV2(always_apply=True),
-    ])
+class MyTransform:
+    def __init__(self, width=160, height=120, gray=False, augment=False):
+        augmentations = Compose([
+            HueSaturationValue(always_apply=True),
+            RandomSizedCrop(min_max_height=(height // 2, height * 4), height=height, width=width,
+                            w2h_ratio=width / height, always_apply=True),
+            OneOf([MotionBlur(p=0.5), GaussNoise(p=0.5)], p=1),
+        ])
 
-    if label is not None and len(label.shape) >= 2:
-        # Binarize label
-        label[label != 0] = 1
+        self.transform = Compose([
+            augmentations if augment else Resize(height=height, width=width, always_apply=True),
+            ToGray(always_apply=True) if gray else NoOp(always_apply=True),
+            Normalize(always_apply=True),
+            ToTensorV2(),
+        ])
 
-        augmented = aug(image=img, mask=label)
-        img = augmented['image']
-        label = augmented['mask'].squeeze().long()
-    else:
-        augmented = aug(image=img)
-        img = augmented['image']
+    def __call__(self, img, label=None):
+        if label is not None and len(label.shape) >= 2:
+            # Binarize label
+            label[label != 0] = 1
 
-    return img, label
+            augmented = self.transform(image=img, mask=label)
+            img = augmented['image']
+            label = augmented['mask'].squeeze().long()
+        else:
+            augmented = self.transform(image=img)
+            img = augmented['image']
+
+        return img, label

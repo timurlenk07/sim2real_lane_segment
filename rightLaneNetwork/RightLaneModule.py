7681824011f836910ee57379ee301a797d4a5704
Timurlenk07 <tmarcikka@gmail.com>
2021-02-25 13:29:20 +0100

    Refactored baseline training script

--- rightLaneNetwork/RightLaneModule.py
+++ rightLaneNetwork/RightLaneModule.py
@@ -17,48 +17,79 @@ from dataManagement.myTransforms import MyTransform
 from models.FCDenseNet.tiramisu import FCDenseNet57Base, FCDenseNet57Classifier
 
 
-class RightLaneModule(pl.LightningModule):
-    def __init__(self, *, dataPath=None, width=160, height=120, gray=False,
-                 augment=False, batch_size=32, lr=1e-3, decay=1e-4, lrRatio=1e3, **kwargs):
-        super().__init__()
-
-        self.dataPath = dataPath
+class RightLaneDataModule(pl.LightningDataModule):
+    def __init__(self, *, dataPath=None, width=160, height=120, gray=False, augment=False, batch_size=1, num_workers=1):
+        super().__init__(
+            train_transforms=MyTransform(width=width, height=height, gray=gray, augment=augment),
+            val_transforms=MyTransform(width=width, height=height, gray=gray, augment=False),
+            test_transforms=MyTransform(width=width, height=height, gray=gray, augment=False),
+            dims=(width, height, 1 if gray else 3)
+        )
         self.dataSets = dict()
+        self.dataPath = dataPath if dataPath is not None else os.getcwd()
+        self.augment = augment
+        self.batch_size = batch_size
+        self.num_workers = num_workers
 
-        # Network parameters
-        self.width, self.height = width, height
-        self.grayscale = gray
+    @classmethod
+    def add_argparse_args(cls, parent_parser: ArgumentParser) -> ArgumentParser:
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        dmGroup = parser.add_argument_group('DataModule', 'Parameters defining data handling')
+
+        dmGroup.add_argument('--gray', action='store_true', help="Convert input image to grayscale")
+        dmGroup.add_argument('--width', type=int, default=160, help="Resize width of input images of the network")
+        dmGroup.add_argument('--height', type=int, default=120, help="Resize height of input images of the network")
+        dmGroup.add_argument('--augment', action='store_true', help="Use data augmentation on training set")
+        dmGroup.add_argument('-b', '--batch_size', type=int, default=32, help="Input batch size")
+
+        return parser
+
+    def setup(self, stage=None):
+        self.dataSets['train'] = RightLaneDataset(os.path.join(self.dataPath, 'train'), self.train_transforms,
+                                                  haveLabels=True)
+        self.dataSets['valid'] = RightLaneDataset(os.path.join(self.dataPath, 'valid'), self.val_transforms,
+                                                  haveLabels=True)
+        self.dataSets['test'] = RightLaneDataset(os.path.join(self.dataPath, 'test'), self.test_transforms,
+                                                 haveLabels=True)
+
+    def train_dataloader(self) -> DataLoader:
+        return DataLoader(self.dataSets['train'], batch_size=self.batch_size, shuffle=True,
+                          num_workers=self.num_workers)
+
+    def val_dataloader(self) -> DataLoader:
+        return DataLoader(self.dataSets['valid'], batch_size=self.batch_size, shuffle=False,
+                          num_workers=self.num_workers)
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.dataSets['test'], batch_size=self.batch_size, shuffle=False,
+                          num_workers=self.num_workers)
+
+
+class RightLaneModule(pl.LightningModule):
+    def __init__(self, *, lr=1e-3, decay=1e-4, lrRatio=1e3):
+        super().__init__()
 
         # Create network parts
         self.featureExtractor = FCDenseNet57Base()
         self.classifier = FCDenseNet57Classifier(n_classes=2)
 
         # Training parameters
-        self.augment = augment
-        self.batch_size = batch_size
         self.lr = lr
         self.decay = decay
         self.lrRatio = lrRatio
 
         # Save hyperparameters
-        self.save_hyperparameters('width', 'height', 'gray', 'augment', 'batch_size', 'lr', 'decay', 'lrRatio')
+        self.save_hyperparameters('lr', 'decay', 'lrRatio')
 
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        argGroup = parser.add_argument_group('LightningModule', 'Parameters defining network training')
 
-        # Network parameters
-        parser.add_argument('--gray', action='store_true', help="Convert input image to grayscale")
-        parser.add_argument('--width', type=int, default=160, help="Resize width of input images of the network")
-        parser.add_argument('--height', type=int, default=120, help="Resize height of input images of the network")
-
-        # Training parameters
-        parser.add_argument('--augment', action='store_true', help="Use data augmentation on training set")
-        parser.add_argument('-lr', '--learningRate', type=float, default=1e-3, help="Starting learning rate")
-        parser.add_argument('--decay', type=float, default=1e-4, help="L2 weight decay value")
-        parser.add_argument('--lrRatio', type=float, default=1000,
-                            help="Ratio of maximum and minimum of learning rate for cosine LR scheduler")
-        parser.add_argument('-b', '--batch_size', type=int, default=32, help="Input batch size")
+        argGroup.add_argument('-lr', '--learningRate', type=float, default=1e-3, help="Starting learning rate")
+        argGroup.add_argument('--decay', type=float, default=1e-4, help="L2 weight decay value")
+        argGroup.add_argument('--lrRatio', type=float, default=1000,
+                              help="Ratio of maximum and minimum of learning rate for cosine LR scheduler")
 
         return parser
 
@@ -66,31 +97,6 @@ class RightLaneModule(pl.LightningModule):
         x = self.featureExtractor(x)
         x = self.classifier(x)
         return x
-
-    def prepare_data(self):
-        trainTransform = MyTransform(width=self.width, height=self.height, gray=self.grayscale, augment=self.augment)
-        testTransform = MyTransform(width=self.width, height=self.height, gray=self.grayscale, augment=False)
-
-        self.dataSets['train'] = RightLaneDataset(os.path.join(self.dataPath, 'source'), trainTransform,
-                                                  haveLabels=True)
-        self.dataSets['valid'] = RightLaneDataset(os.path.join(self.dataPath, 'target', 'train'), testTransform,
-                                                  haveLabels=True)
-        self.dataSets['test'] = RightLaneDataset(os.path.join(self.dataPath, 'target', 'test'), testTransform,
-                                                 haveLabels=True)
-
-    def train_dataloader(self):
-        return DataLoader(self.dataSets['train'], batch_size=self.batch_size, shuffle=True, num_workers=8)
-
-    def val_dataloader(self):
-        return DataLoader(self.dataSets['valid'], batch_size=self.batch_size, shuffle=False, num_workers=8)
-
-    def test_dataloader(self):
-        return DataLoader(self.dataSets['test'], batch_size=self.batch_size, shuffle=False, num_workers=8)
-
-    def configure_optimizers(self):
-        optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=self.decay)
-        scheduler = CosineAnnealingLR(optimizer, 25, eta_min=self.lr / self.lrRatio)
-        return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -103,22 +109,38 @@ class RightLaneModule(pl.LightningModule):
         _, labels_hat = torch.max(outputs, 1)
         train_acc = accuracy(labels_hat, y) * 100
 
-        progress_bar = {
-            'tr_acc': train_acc
-        }
-        logs = {
-            'train_loss': loss,
-            'train_acc': train_acc
-        }
+        self.log('tr_loss', train_acc)
+        self.log('tr_acc', train_acc)
 
-        output = OrderedDict({
-            'loss': loss,
-            'progress_bar': progress_bar,
-            'log': logs
-        })
-        return output
+        return loss
 
     def validation_step(self, batch, batch_idx):
+        return self.evaluate_batch(batch)
+
+    def validation_epoch_end(self, outputs):
+        logs = self.summarize_evaluation_results(outputs)
+        self.log('val_loss', logs['loss'])
+        self.log('val_acc', logs['acc'])
+        self.log('val_dice', logs['dice'])
+        self.log('val_iou', logs['iou'])
+        self.log('step', self.current_epoch)
+
+    def test_step(self, batch, batch_idx):
+        return self.evaluate_batch(batch)
+
+    def test_epoch_end(self, outputs):
+        logs = self.summarize_evaluation_results(outputs)
+        self.log('test_loss', logs['loss'])
+        self.log('test_acc', logs['acc'])
+        self.log('test_dice', logs['dice'])
+        self.log('test_iou', logs['iou'])
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=self.decay)
+        scheduler = CosineAnnealingLR(optimizer, 25, eta_min=self.lr / self.lrRatio)
+        return [optimizer], [scheduler]
+
+    def evaluate_batch(self, batch):
         x, y = batch
 
         # Forward propagation, loss calculation
@@ -138,39 +160,19 @@ class RightLaneModule(pl.LightningModule):
 
         return output
 
-    def validation_epoch_end(self, outputs):
+    def summarize_evaluation_results(self, outputs):
         total_weight = sum(x['weight'] for x in outputs)
-        val_loss = sum([x['loss'] for x in outputs]) / total_weight
-        val_acc = sum([x['acc'] for x in outputs]) / total_weight * 100.0
-        val_dice = sum([x['dice'] for x in outputs]) / total_weight
-        val_iou = sum([x['iou'] for x in outputs]) / total_weight * 100.0
+        loss = sum([x['loss'] for x in outputs]) / total_weight
+        acc = sum([x['acc'] for x in outputs]) / total_weight * 100.0
+        dice = sum([x['dice'] for x in outputs]) / total_weight
+        iou = sum([x['iou'] for x in outputs]) / total_weight * 100.0
 
-        logs = {
-            'val_loss': val_loss,
-            'val_acc': val_acc,
-            'val_dice': val_dice,
-            'val_iou': val_iou,
-            'step': self.current_epoch,
+        return {
+            'loss': loss,
+            'acc': acc,
+            'dice': dice,
+            'iou': iou,
         }
-        return {'progress_bar': logs, 'log': logs}
-
-    def test_step(self, batch, batch_idx):
-        return self.validation_step(batch, batch_idx)
-
-    def test_epoch_end(self, outputs):
-        total_weight = sum(x['weight'] for x in outputs)
-        test_loss = sum([x['loss'] for x in outputs]) / total_weight
-        test_acc = sum([x['acc'] for x in outputs]) / total_weight * 100.0
-        test_dice = sum([x['dice'] for x in outputs]) / total_weight
-        test_iou = sum([x['iou'] for x in outputs]) / total_weight * 100.0
-
-        logs = {
-            'test_loss': test_loss,
-            'test_acc': test_acc,
-            'test_dice': test_dice,
-            'test_iou': test_iou,
-        }
-        return {'progress_bar': logs, 'log': logs}
 
 
 def main(args, model_name: str, reproducible: bool, comet: bool):
@@ -202,12 +204,13 @@ def main(args, model_name: str, reproducible: bool, comet: bool):
     )
     args.checkpoint_callback = model_checkpoint
 
-    model = RightLaneModule(**vars(args))
+    data = RightLaneDataModule(dataPath=args.dataPath, augment=True, batch_size=64, num_workers=8)
+    model = RightLaneModule(lr=args.learningRate, lrRatio=args.lrRatio, decay=args.decay)
 
     # Parse all trainer options available from the command line
     trainer = pl.Trainer.from_argparse_args(args)
 
-    trainer.fit(model)
+    trainer.fit(model, datamodule=data)
 
     # Reload best model
     model = RightLaneModule.load_from_checkpoint(model_checkpoint.kth_best_model_path, dataPath=args.dataPath)
@@ -222,7 +225,7 @@ def main(args, model_name: str, reproducible: bool, comet: bool):
         comet_logger.experiment.log_model(model_name + '_weights', weights_path)
 
     # Perform testing
-    trainer.test(model)
+    trainer.test(model, datamodule=data)
 
 
 if __name__ == '__main__':
@@ -238,6 +241,7 @@ if __name__ == '__main__':
                         help="Set seed to 42 and deterministic and benchmark to True.")
 
     # Add model arguments to parser
+    parser = RightLaneDataModule.add_argparse_args(parser)
     parser = RightLaneModule.add_model_specific_args(parser)
 
     # Adds all the trainer options as default arguments (like max_epochs)

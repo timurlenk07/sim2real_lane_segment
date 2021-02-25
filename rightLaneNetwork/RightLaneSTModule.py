4d35ca9b8c984fc4fd67e633e05d815b98fb8b9e
Timurlenk07 <tmarcikka@gmail.com>
2021-02-25 18:52:51 +0100

    Refactored RightLaneSTModule.py completely

--- rightLaneNetwork/RightLaneSTModule.py
+++ rightLaneNetwork/RightLaneSTModule.py
@@ -1,64 +1,42 @@
 import os
 from argparse import ArgumentParser
-from collections import OrderedDict
 
-import pytorch_lightning as pl
 import torch
-from pytorch_lightning import seed_everything
+from pytorch_lightning import LightningModule, loggers
+from pytorch_lightning import seed_everything, Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.metrics.functional import accuracy, dice_score, iou
 from torch.nn.functional import cross_entropy
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import CosineAnnealingLR
-from torch.utils.data import DataLoader, ConcatDataset, WeightedRandomSampler
 
-from dataManagement.myDatasets import RightLaneDataset
-from dataManagement.myTransforms import MyTransform
+from dataManagement.dataModules import TwoDomainDM
 from models.FCDenseNet.tiramisu import FCDenseNet57Base, FCDenseNet57Classifier
 
 
-class RightLaneSTModule(pl.LightningModule):
-    def __init__(self, *, dataPath=None, width=160, height=120, gray=False,
-                 augment=False, batch_size=32, lr=1e-3, decay=1e-4, lrRatio=1e3, **kwargs):
+class RightLaneSTModule(LightningModule):
+    def __init__(self, lr=1e-3, decay=1e-4, lrRatio=1e3, num_cls=2):
         super().__init__()
-
-        self.dataPath = dataPath
-        self.dataSets = dict()
-
-        # Network parameters
-        self.width, self.height = width, height
-        self.grayscale = gray
+        self.save_hyperparameters('lr', 'decay', 'lrRatio')
 
         # Create network parts
         self.featureExtractor = FCDenseNet57Base()
-        self.classifier = FCDenseNet57Classifier(n_classes=2)
+        self.classifier = FCDenseNet57Classifier(n_classes=num_cls)
 
         # Training parameters
-        self.augment = augment
-        self.batch_size = batch_size
         self.lr = lr
         self.decay = decay
         self.lrRatio = lrRatio
 
-        # Save hyperparameters
-        self.save_hyperparameters('width', 'height', 'gray', 'augment', 'batch_size', 'lr', 'decay', 'lrRatio')
-
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        argGroup = parser.add_argument_group('LightningModule', 'Parameters defining network training')
 
-        # Network parameters
-        parser.add_argument('--gray', action='store_true', help="Convert input image to grayscale")
-        parser.add_argument('--width', type=int, default=160, help="Resize width of input images of the network")
-        parser.add_argument('--height', type=int, default=120, help="Resize height of input images of the network")
-
-        # Training parameters
-        parser.add_argument('--augment', action='store_true', help="Use data augmentation on training set")
-        parser.add_argument('-lr', '--learningRate', type=float, default=1e-3, help="Starting learning rate")
-        parser.add_argument('--decay', type=float, default=1e-4, help="L2 weight decay value")
-        parser.add_argument('--lrRatio', type=float, default=1000,
-                            help="Ratio of maximum and minimum of learning rate for cosine LR scheduler")
-        parser.add_argument('-b', '--batch_size', type=int, default=32, help="Input batch size")
+        argGroup.add_argument('-lr', '--learningRate', type=float, default=1e-3, help="Starting learning rate")
+        argGroup.add_argument('--decay', type=float, default=1e-4, help="L2 weight decay value")
+        argGroup.add_argument('--lrRatio', type=float, default=1000,
+                              help="Ratio of maximum and minimum of learning rate for cosine LR scheduler")
 
         return parser
 
@@ -66,42 +44,6 @@ class RightLaneSTModule(pl.LightningModule):
         x = self.featureExtractor(x)
         x = self.classifier(x)
         return x
-
-    def prepare_data(self):
-        trainTransform = MyTransform(width=self.width, height=self.height, gray=self.grayscale, augment=self.augment)
-        testTransform = MyTransform(width=self.width, height=self.height, gray=self.grayscale, augment=False)
-
-        self.dataSets['source'] = RightLaneDataset(os.path.join(self.dataPath, 'source'), trainTransform,
-                                                   haveLabels=True)
-        self.dataSets['targetTrain'] = RightLaneDataset(os.path.join(self.dataPath, 'target', 'train'),
-                                                        trainTransform, haveLabels=True)
-        self.dataSets['targetUnlabelled'] = RightLaneDataset(os.path.join(self.dataPath, 'target', 'unlabelled'),
-                                                             trainTransform, haveLabels=False)
-        self.dataSets['targetTest'] = RightLaneDataset(os.path.join(self.dataPath, 'target', 'test'), testTransform,
-                                                       haveLabels=True)
-
-    def train_dataloader(self):
-        sourceSet = self.dataSets['source']
-        targetSet = self.dataSets['targetTrain']
-        STSet = ConcatDataset([sourceSet, targetSet])
-
-        source_weights = [1.0 / len(sourceSet) for _ in range(len(sourceSet))]
-        target_weights = [1.0 / len(targetSet) for _ in range(len(targetSet))]
-        weights = [*source_weights, *target_weights]
-
-        sampler = WeightedRandomSampler(weights=weights, num_samples=len(STSet), replacement=True)
-        return DataLoader(STSet, sampler=sampler, batch_size=self.batch_size, num_workers=8)
-
-    def val_dataloader(self):
-        return self.test_dataloader()
-
-    def test_dataloader(self):
-        return DataLoader(self.dataSets['targetTest'], batch_size=self.batch_size, shuffle=False, num_workers=8)
-
-    def configure_optimizers(self):
-        optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=self.decay)
-        scheduler = CosineAnnealingLR(optimizer, 25, eta_min=self.lr / self.lrRatio)
-        return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -114,22 +56,38 @@ class RightLaneSTModule(pl.LightningModule):
         _, labels_hat = torch.max(outputs, 1)
         train_acc = accuracy(labels_hat, y) * 100
 
-        progress_bar = {
-            'tr_acc': train_acc
-        }
-        logs = {
-            'train_loss': loss,
-            'train_acc': train_acc
-        }
+        self.log('tr_loss', loss)
+        self.log('tr_acc', train_acc, prog_bar=True)
 
-        output = OrderedDict({
-            'loss': loss,
-            'progress_bar': progress_bar,
-            'log': logs
-        })
-        return output
+        return loss
 
     def validation_step(self, batch, batch_idx):
+        return self.evaluate_batch(batch)
+
+    def validation_epoch_end(self, outputs):
+        logs = self.summarize_evaluation_results(outputs)
+        self.log('val_loss', logs['loss'])
+        self.log('val_acc', logs['acc'], prog_bar=True, logger=True)
+        self.log('val_dice', logs['dice'])
+        self.log('val_iou', logs['iou'], prog_bar=True, logger=True)
+        # self.log('step', self.current_epoch)
+
+    def test_step(self, batch, batch_idx):
+        return self.evaluate_batch(batch)
+
+    def test_epoch_end(self, outputs):
+        logs = self.summarize_evaluation_results(outputs)
+        self.log('test_loss', logs['loss'])
+        self.log('test_acc', logs['acc'])
+        self.log('test_dice', logs['dice'])
+        self.log('test_iou', logs['iou'])
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=self.decay)
+        scheduler = CosineAnnealingLR(optimizer, 25, eta_min=self.lr / self.lrRatio)
+        return [optimizer], [scheduler]
+
+    def evaluate_batch(self, batch):
         x, y = batch
 
         # Forward propagation, loss calculation
@@ -139,52 +97,30 @@ class RightLaneSTModule(pl.LightningModule):
         _, labels_hat = torch.max(outputs, 1)
 
         weight = x.shape[0]
-        output = OrderedDict({
+        return {
             'loss': loss * weight,
             'acc': accuracy(labels_hat, y) * weight,
             'dice': dice_score(outputs, y) * weight,
             'iou': iou(labels_hat, y) * weight,
             'weight': weight,
-        })
-
-        return output
-
-    def validation_epoch_end(self, outputs):
-        total_weight = sum(x['weight'] for x in outputs)
-        val_loss = sum([x['loss'] for x in outputs]) / total_weight
-        val_acc = sum([x['acc'] for x in outputs]) / total_weight * 100.0
-        val_dice = sum([x['dice'] for x in outputs]) / total_weight
-        val_iou = sum([x['iou'] for x in outputs]) / total_weight * 100.0
-
-        logs = {
-            'val_loss': val_loss,
-            'val_acc': val_acc,
-            'val_dice': val_dice,
-            'val_iou': val_iou,
-            'step': self.current_epoch,
         }
-        return {'progress_bar': logs, 'log': logs}
 
-    def test_step(self, batch, batch_idx):
-        return self.validation_step(batch, batch_idx)
-
-    def test_epoch_end(self, outputs):
+    def summarize_evaluation_results(self, outputs):
         total_weight = sum(x['weight'] for x in outputs)
-        test_loss = sum([x['loss'] for x in outputs]) / total_weight
-        test_acc = sum([x['acc'] for x in outputs]) / total_weight * 100.0
-        test_dice = sum([x['dice'] for x in outputs]) / total_weight
-        test_iou = sum([x['iou'] for x in outputs]) / total_weight * 100.0
+        loss = sum([x['loss'] for x in outputs]) / total_weight
+        acc = sum([x['acc'] for x in outputs]) / total_weight * 100.0
+        dice = sum([x['dice'] for x in outputs]) / total_weight
+        iou = sum([x['iou'] for x in outputs]) / total_weight * 100.0
 
-        logs = {
-            'test_loss': test_loss,
-            'test_acc': test_acc,
-            'test_dice': test_dice,
-            'test_iou': test_iou,
+        return {
+            'loss': loss,
+            'acc': acc,
+            'dice': dice,
+            'iou': iou,
         }
-        return {'progress_bar': logs, 'log': logs}
 
 
-def main(args, model_name: str, reproducible: bool, comet: bool):
+def main(args, model_name: str, reproducible: bool, comet: bool, wandb: bool):
     if reproducible:
         seed_everything(42)
         args.deterministic = True
@@ -194,46 +130,44 @@ def main(args, model_name: str, reproducible: bool, comet: bool):
         args.default_root_dir = 'results'
 
     if comet:
-        comet_logger = pl.loggers.CometLogger(
+        from pytorch_lightning.loggers import CometLogger
+        comet_logger = loggers.CometLogger(
             api_key=os.environ.get('COMET_API_KEY'),
             workspace=os.environ.get('COMET_WORKSPACE'),  # Optional
             project_name=os.environ.get('COMET_PROJECT_NAME'),  # Optional
             experiment_name=model_name  # Optional
         )
         args.logger = comet_logger
+    if wandb:
+        from pytorch_lightning.loggers import WandbLogger
+        wandb_logger = WandbLogger(project=os.environ.get('WANDB_PROJECT_NAME'), log_model=True, sync_step=True)
+        args.logger = wandb_logger
 
     # Save best model
     model_checkpoint = ModelCheckpoint(
-        filepath=os.path.join(args.default_root_dir, model_name + '.ckpt'),
+        filename=model_name + '_{epoch}',
         save_top_k=1,
-        verbose=False,
         monitor='val_iou',
         mode='max',
-        prefix=model_name + str(os.getpid())
     )
     args.checkpoint_callback = model_checkpoint
 
-    model = RightLaneSTModule(**vars(args))
+    data = TwoDomainDM(dataPath=args.dataPath, augment=args.augment, batch_size=args.batch_size, num_workers=8)
+    model = RightLaneSTModule(lr=args.learningRate, lrRatio=args.lrRatio, decay=args.decay, num_cls=4)
 
     # Parse all trainer options available from the command line
-    trainer = pl.Trainer.from_argparse_args(args)
-
-    trainer.fit(model)
+    trainer = Trainer.from_argparse_args(args)
+    trainer.fit(model, datamodule=data)
 
     # Reload best model
-    model = RightLaneSTModule.load_from_checkpoint(model_checkpoint.kth_best_model_path, dataPath=args.dataPath)
+    model = RightLaneSTModule.load_from_checkpoint(model_checkpoint.best_model_path, dataPath=args.dataPath, num_cls=4)
 
-    # Save checkpoint and weights
-    ckpt_path = os.path.join(args.default_root_dir, model_name + '.ckpt')
-    weights_path = os.path.join(args.default_root_dir, model_name + '_weights.pth')
-    trainer.save_checkpoint(ckpt_path)
-    torch.save(model.state_dict(), weights_path)
-    if args.comet:
-        comet_logger.experiment.log_model(model_name + '_ckpt', ckpt_path)
-        comet_logger.experiment.log_model(model_name + '_weights', weights_path)
+    # Upload weights
+    if comet:
+        comet_logger.experiment.log_model(model_name + '_weights', model_checkpoint.best_model_path)
 
     # Perform testing
-    trainer.test(model)
+    trainer.test(model, datamodule=data)
 
 
 if __name__ == '__main__':
@@ -243,16 +177,21 @@ if __name__ == '__main__':
     parser.add_argument('--dataPath', type=str, help="Path of database root")
 
     parser.add_argument('--comet', action='store_true', help='Define flag in order to use Comet.ml as logger.')
+    parser.add_argument('--wandb', action='store_true', help='Define flag in order to use WandB as logger.')
     parser.add_argument('--model_name', type=str, default='sandt', help='Model identifier for logging and checkpoints.')
     parser.add_argument('--reproducible', action='store_true',
                         help="Set seed to 42 and deterministic and benchmark to True.")
 
     # Add model arguments to parser
+    parser = TwoDomainDM.add_argparse_args(parser)
     parser = RightLaneSTModule.add_model_specific_args(parser)
 
     # Adds all the trainer options as default arguments (like max_epochs)
-    parser = pl.Trainer.add_argparse_args(parser)
+    parser = Trainer.add_argparse_args(parser)
 
     args = parser.parse_args()
 
-    main(args, model_name=args.model_name, reproducible=args.reproducible, comet=args.comet)
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    main(args, model_name=args.model_name, reproducible=args.reproducible, comet=args.comet, wandb=args.wandb)
--- rightLaneNetwork/dataManagement/dataModules.py
+++ rightLaneNetwork/dataManagement/dataModules.py
@@ -2,7 +2,7 @@ import os
 from argparse import ArgumentParser
 
 from pytorch_lightning import LightningDataModule
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset, WeightedRandomSampler
 
 from .myDatasets import RightLaneDataset
 from .myTransforms import MyTransform
@@ -54,3 +54,59 @@ class SimulatorDataModule(LightningDataModule):
     def test_dataloader(self) -> DataLoader:
         return DataLoader(self.dataSets['test'], batch_size=self.batch_size, shuffle=False,
                           num_workers=self.num_workers)
+
+
+class TwoDomainDM(LightningDataModule):
+    def __init__(self, *, dataPath=None, width=160, height=120, gray=False, augment=False, batch_size=1, num_workers=1):
+        super().__init__(
+            train_transforms=MyTransform(width=width, height=height, gray=gray, augment=augment),
+            val_transforms=MyTransform(width=width, height=height, gray=gray, augment=False),
+            test_transforms=MyTransform(width=width, height=height, gray=gray, augment=False),
+            dims=(width, height, 1 if gray else 3)
+        )
+        self.dataSets = dict()
+        self.dataPath = dataPath if dataPath is not None else os.getcwd()
+        self.augment = augment
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    @classmethod
+    def add_argparse_args(cls, parent_parser: ArgumentParser) -> ArgumentParser:
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        dmGroup = parser.add_argument_group('DataModule', 'Parameters defining data handling')
+
+        dmGroup.add_argument('--gray', action='store_true', help="Convert input image to grayscale")
+        dmGroup.add_argument('--width', type=int, default=160, help="Resize width of input images of the network")
+        dmGroup.add_argument('--height', type=int, default=120, help="Resize height of input images of the network")
+        dmGroup.add_argument('--augment', action='store_true', help="Use data augmentation on training set")
+        dmGroup.add_argument('-b', '--batch_size', type=int, default=32, help="Input batch size")
+
+        return parser
+
+    def setup(self, stage=None):
+        self.dataSets['source'] = RightLaneDataset(os.path.join(self.dataPath, 'source'), self.train_transforms,
+                                                   haveLabels=True)
+        self.dataSets['targetTrain'] = RightLaneDataset(os.path.join(self.dataPath, 'target', 'train'),
+                                                        self.train_transforms, haveLabels=True)
+        self.dataSets['targetUnlabelled'] = RightLaneDataset(os.path.join(self.dataPath, 'target', 'unlabelled'),
+                                                             self.train_transforms, haveLabels=False)
+        self.dataSets['targetTest'] = RightLaneDataset(os.path.join(self.dataPath, 'target', 'test'),
+                                                       self.test_transforms, haveLabels=True)
+
+    def train_dataloader(self) -> DataLoader:
+        sourceSet = self.dataSets['source']
+        targetSet = self.dataSets['targetTrain']
+        STSet = ConcatDataset([sourceSet, targetSet])
+
+        source_weights = [1.0 / len(sourceSet) for _ in range(len(sourceSet))]
+        target_weights = [1.0 / len(targetSet) for _ in range(len(targetSet))]
+        weights = [*source_weights, *target_weights]
+
+        sampler = WeightedRandomSampler(weights=weights, num_samples=len(STSet), replacement=True)
+        return DataLoader(STSet, sampler=sampler, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def val_dataloader(self) -> DataLoader:
+        return self.test_dataloader()
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(self.dataSets['targetTest'], batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
